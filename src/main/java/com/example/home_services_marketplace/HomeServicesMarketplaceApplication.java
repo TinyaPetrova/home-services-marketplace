02893c6728b43adb784a4bcb08b63cb897da4905
Ustinya Petrova <ust.petrova@gmail.com>
2026-06-24 21:42:03 +0200

    initial spring boot setup

---
+++ src/main/java/com/example/home_services_marketplace/HomeServicesMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package com.example.home_services_marketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeServicesMarketplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeServicesMarketplaceApplication.class, args);
+	}
+
+}
