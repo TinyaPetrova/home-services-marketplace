c1f96baa2546abb342cdf0a0c8ac73c9dde37de4
TinyaPetrova <ust.petrova@gmail.com>
2026-06-27 02:29:48 +0200

    update user entity fields

--- src/main/java/com/example/home_services_marketplace/entity/User.java
+++ src/main/java/com/example/home_services_marketplace/entity/User.java
@@ -1,22 +1,48 @@
 package com.example.home_services_marketplace.entity;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
-import java.util.Date;
-import org.springframework.web.bind.annotation.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "users")
 public class User {
+
   @Id
-  int id;
-  String firstName;
-  String lastName;
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  long id;
+
+  private String firstName;
+
+  private String lastName;
+
   @Email
-  String email;
-  String password;
-  String phone;
-  Date createdAt;
-  Date updatedAt;
+  @Column(unique = true, nullable = true)
+  private String email;
+
+  @Column(nullable = false)
+  private String password;
+
+  private String phone;
+
+  private LocalDateTime createdAt;
+
+  private LocalDateTime updatedAt;
+
+  @PrePersist
+  protected void createTime() {
+    createdAt = LocalDateTime.now();
+    updatedAt = LocalDateTime.now();
+  }
+
+  @PreUpdate
+  protected void updateTime() {
+    updatedAt = LocalDateTime.now();
+  }
 }
